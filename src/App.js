fb00bacf1daaf4f279cc0b65890e32ef912823e9
suth <iansutherland.az@gmail.com>
2021-03-07 11:40:46 -0700

    list all questions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {Paper, Container, Button} from '@material-ui/core';
+import data from './data/Q&A.json';
+import {useState} from 'react';
 
 function App() {
+  const stuff = Object.keys(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        {stuff.map((x, i) => <PaperWrap data={data[x]} number={i}></PaperWrap>)}
+      </Container>
+    </div>
+  );
+}
+
+function PaperWrap(props){
+  const data = props.data;
+  const Qindex = props.number;
+  data.example = data.example == "" ? "no example set at this time" : data.example;
+  const [showExample, setShowExample] = useState(false);
+  function showNow(){
+    setShowExample(!showExample);
+  }
+  return (
+    <div className="margind">
+      <Paper>
+        <div>{Qindex}</div>
+        {data.question}
+        <br />
+        <div className={showExample ? "margind": ""}>{showExample ? data.example : ""}</div>
+        <br />
+        <Button color="primary" variant="contained" onClick={showNow}>See example</Button>
+        </Paper>
     </div>
   );
 }
